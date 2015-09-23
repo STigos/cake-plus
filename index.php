41143b2a78e26fb1697a1eb2b572c649d5feed6c
stigos <stigos@192.168.1.3>
2015-09-23 12:52:59 +0500

    первая фиксация

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+
+    <title>Кейк-плюс торты на заказ в г. Миассе</title>
+  
+    <script src="javascripts/jquery-2.1.4.js" ></script>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <script src="javascripts/bootstrap.js"></script>
+  </head>
+
+  <body>    
+      <div class="container">
+        <div class="navbar-inverse  navbar-static-top " role="navigation">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+                <a class="navbar-brand" href="#"><span class="glyphicon-home"></span>Cake-plus</a>
+            </div>
+            <div class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                  <li><a href="test.php">Test</a></li>
+                <li><a href="#about"></a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li class="divider"></li>
+                    <li class="dropdown-header">Nav header</li>
+                    <li><a href="#">Separated link</a></li>
+                    <li><a href="#">One more separated link</a></li>
+                  </ul>
+                 <li><a href="#contact">О нас</a></li>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+          <div class="container">
+               <h2 class="text-center">Cake-plus</h2>
+              <div class="col-lg-4 col-sm-12 col-md-6">
+                  <h4 class="caption">Торты на заказ</h4>
+                  <a href="//..">
+                      <img src="images/75943206.gif" alt="" class="img-thumbnail">
+                  </a></div>
+              <div class="col-lg-4 col-sm-12 col-md-6">
+                  <h4 class="caption">Пироги</h4>
+                  <a href="//..">
+                      <img src="images/75943206.gif" alt="" class="img-thumbnail">
+                  </a></div>
+              <div class="col-lg-4 col-sm-12 col-md-6">
+                  <h4 class="caption">Кап-кейки</h4>
+                  <a href="//..">
+                      <img src="images/75943206.gif" alt="" class="img-thumbnail">
+                  </a></div>
+               <script >
+               var sting1 = "Строка!";
+               function InsertToHtml(){
+                   document.write(sting1);
+                  
+               }
+               </script>
+               <div class="col-lg-12 col-sm-12 col-xs-12">
+                   <p id="forInner">dkfjkl</p>
+                   <button type="button" onclick="forInner()">Push me!</button>
+               </div>
+          </div>
+         
+      </div>
+   
+
+
+    
+    
+  </body>
+</html>
---
+++ test.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+
+<html lang="RU">
+    <head>
+        <meta charset="UTF-8">
+        <title>Страница для тестирования PHP кода</title>
+         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+        <script src="javascripts/jquery-2.1.4.js" ></script>
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <script src="javascripts/bootstrap.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+        <div class="navbar-inverse  navbar-static-top " role="navigation">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+                <a class="navbar-brand" href="index.php"><span class="glyphicon-home"></span>Cake-plus</a>
+            </div>
+            <div class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                  <li><a href="test.php">Test</a></li>
+                <li><a href="#about"></a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li class="divider"></li>
+                    <li class="dropdown-header">Nav header</li>
+                    <li><a href="#">Separated link</a></li>
+                    <li><a href="#">One more separated link</a></li>
+                  </ul>
+                 <li><a href="#contact">О нас</a></li>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <?php
+         
+        ?>
+            <script>
+                var name = 'Привет!';
+                function set23(){
+                    document.write(name);
+                }
+            </script>
+            <div class="row">
+                <div class="col-lg-6 col-sm-12"><p>Некоторый текст</p>
+                    <button onclick="set23()">Нажми</button></div>
+                <div class="col-lg-6 col-sm-12"><p>Некоторый текст</p></div>
+                
+                
+            </div>
+    </body>
+</html>
